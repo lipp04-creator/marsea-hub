6d69a18431922421bd0bf2e9fb3be5ecc5a4ceed
lipp04-creator <alifakbar04032005@gmail.com>
2025-11-16 16:40:15 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,1 @@
-// db.js
-const mysql = require("mysql2/promise");
-require("dotenv").config();
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",   // ← ini yang dipakai
-  database: process.env.DB_NAME || "marsea_backend",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-module.exports = pool;
+remove backend/ from frontend repo
